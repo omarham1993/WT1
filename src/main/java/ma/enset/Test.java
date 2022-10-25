61b1b1f0a1d90e31b7e589b933a3be89bf8fb359
Omar <omarhammami2105@gmail.com>
2022-10-25 00:56:19 -0400

    Comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,12 @@
+
+/*
+ comment
+ */
+
 package ma.enset;
 public class Test{
     public static void main(String[]args){
+        //....................
         System.out.println("Hello");
     }
 }
